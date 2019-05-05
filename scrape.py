b787b36c33653ca511c00048427c9686b0dbb620
yusuf-uthman <yusufuthman57@gmail.com>
2019-05-05 22:02:50 +0100

    creates a connection to the website and grabs each product container

--- scrape.py
+++ scrape.py
@@ -1,3 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as BeautifulSoup
 from csv import writer
+response = requests.get("https://www.newegg.com/Network-Print-Servers/SubCategory/ID-387?Tid=158135")
+soup = BeautifulSoup(response.text, "html.parser")
+containers = soup.findAll(class_="item-container")
+print(containers)

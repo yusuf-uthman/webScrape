f33f682137c8a21dc195cd59391130f4ea25b543
yusuf-uthman <yusufuthman57@gmail.com>
2019-05-06 16:17:52 +0100

    i ran the which script created a csv file containing the scraped data

--- scrape.py
+++ scrape.py
@@ -7,19 +7,21 @@ response = requests.get("https://www.newegg.com/Product/ProductList.aspx?Submit=
 page_soup = soup(response.text, "html.parser")
 #Grabs each product 
 containers = page_soup.findAll(class_="item-info")
-
+#creates a file to write the scraped info
 file_name = "products.csv"
+#open the file to write scraped info
 f = open(file_name, "w")
-
+#created headers for the dataset
 headers = "brand, product_name, shipping \n"
 f.write(headers)
-
+#creates a loop that iterates through every div with a class of "item-info" to get the data we need 
 for container in containers:
+    #grabs the brand name
     brand = container.div.a.img["title"] 
-    
+    #grabs the product name
     container_name = container.findAll("a", class_="item-title")
     product = container_name[0].get_text()
-    
+    #grabs the shipping amount
     shipping_container = container.findAll("li", class_="price-ship")
     shipping = shipping_container[0].get_text().strip()
 

8ed4a5aa0b28246debc7e92d947623ad61adc930
yusuf-uthman <yusufuthman57@gmail.com>
2019-05-06 15:45:36 +0100

    grabs the information needed from each product

--- scrape.py
+++ scrape.py
@@ -1,7 +1,26 @@
+#import necessary libraries
 import requests
-from bs4 import BeautifulSoup as BeautifulSoup
+from bs4 import BeautifulSoup as soup
 from csv import writer
-response = requests.get("https://www.newegg.com/Network-Print-Servers/SubCategory/ID-387?Tid=158135")
-soup = BeautifulSoup(response.text, "html.parser")
-containers = soup.findAll(class_="item-container")
-print(containers)
+response = requests.get("https://www.newegg.com/Product/ProductList.aspx?Submit=StoreIM&Depa=1&Category=38")
+#parsing the html of te page
+page_soup = soup(response.text, "html.parser")
+#Grabs each product 
+containers = page_soup.findAll(class_="item-info")
+
+file_name = "products.csv"
+f = open(file_name, "w")
+
+headers = "brand, product_name, shipping /n"
+f.write(headers)
+
+for container in containers:
+    brand = container.div.a.img["title"] 
+    
+    container_name = container.findAll("a", class_="item-title")
+    product = container_name[0].get_text()
+    
+    shipping_container = container.findAll("li", class_="price-ship")
+    shipping = shipping_container[0].get_text().strip()
+
+    f.write(brand +","+ product.replace(",","|") + "," + shipping)
